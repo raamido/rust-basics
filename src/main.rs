5d2402c849aa1ba3a7880e07742bae5e3e201948
Rami <ramiabrahem24@gmail.com>
2023-06-15 22:39:57 +0200

    Variables & Data Types

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 fn main() {
-    /*  Print Macros  */
+    /*  Variables & Data Types  */
 
-    // Print inline
-    print!("[inline] Hello, World");
+    // Variables
+    let name = "rami";
+    let prices = [1000, 2000, 5000, 1000000];
 
-    // Print line
-    println!("[line] Hello, World");
+    // mutable variable (could be modified)
+    let mut ChangeMe = "hi there";
+    ChangeMe = "I've changed";
+
+    // basic data types
+    /*
+       1. Strings "Hello World"
+       2. Chars '😀'
+       3. Integers [i32,i64] 1234...
+       4. Floats [f32, f64] 123.456...
+       5. Boolean true, false
+       6. Array [1,2,3,4,5,6]
+       7. Tuple (1,"hello","2")
+       8. Vector vec![1,2,3,4,5]
+    */
 }
