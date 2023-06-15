789453dd99f8da03b41637b7e429f483b83f20d0
Rami <ramiabrahem24@gmail.com>
2023-06-15 22:43:36 +0200

    Functions

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,9 @@
 fn main() {
-    /*  Variables & Data Types  */
+    /*  Functions  */
+    let sum1 = sum(45, 66);
+    println!("{sum1}")
+}
 
-    // Variables
-    let name = "rami";
-    let prices = [1000, 2000, 5000, 1000000];
-
-    // mutable variable (could be modified)
-    let mut ChangeMe = "hi there";
-    ChangeMe = "I've changed";
-
-    // basic data types
-    /*
-       1. Strings "Hello World"
-       2. Chars '😀'
-       3. Integers [i32,i64] 1234...
-       4. Floats [f32, f64] 123.456...
-       5. Boolean true, false
-       6. Array [1,2,3,4,5,6]
-       7. Tuple (1,"hello","2")
-       8. Vector vec![1,2,3,4,5]
-    */
+fn sum(n1: i32, n2: i32) -> i32 {
+    return n1 + n2;
 }
